65e073e1f4e78cf707092d121490157cdb5d71e7
vutuankiet4599 <vutuankiet4599@gmail.com>
2023-01-03 04:43:30 +0000

    fix kadai1

--- kadai1.rb
+++ kadai1.rb
@@ -1,25 +1,26 @@
+def is_an_integer(value)
+  !!begin
+      Integer(value)
+    rescue StandardError
+      false
+    end
+end
+
 class Menu
   attr_accessor :name, :price
 
-  def get_total_price
-    count = 0
-    print 'Number of Phở: '
-    loop do
-      count = gets.chomp
-      break if (count === count.to_i.to_s) && (count.to_i > 0)
-
-      print 'Must be an integer bigger than 0: '
+  def get_total_price(number)
+    if is_an_integer(number)
+      price = number >= 3 ? @price - 10_000 : @price
+      total_price = price * number
+      "#{name} #{total_price}vnd"
+    else
+      '入力は整数でなければなりません'
     end
-
-    price = @price >= 3 ? @price - 10_000 : @price
-
-    total_price = price * count.to_i
-
-    "#{name} #{total_price}vnd"
   end
 end
 
 menu1 = Menu.new
 menu1.name = 'Phở'
 menu1.price = 30_000
-puts menu1.get_total_price
+puts menu1.get_total_price('q')
