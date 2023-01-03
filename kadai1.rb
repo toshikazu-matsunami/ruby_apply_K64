0b412c510c156cfdac9821494603c49bbc113ebe
vutuankiet4599 <vutuankiet4599@gmail.com>
2023-01-03 05:27:58 +0000

    fix total price

--- kadai1.rb
+++ kadai1.rb
@@ -11,8 +11,8 @@ class Menu
 
   def get_total_price(number)
     if is_an_integer(number)
-      price = number.to_i >= 3 ? @price - 10_000 : @price
-      total_price = price * number.to_i
+      number = number.to_i
+      total_price = number >= 3 ? @price * number - 10_000 : @price * number
       "#{name} #{total_price}vnd"
     else
       '入力は整数でなければなりません'
