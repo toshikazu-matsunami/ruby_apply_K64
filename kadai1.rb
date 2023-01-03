beb259ff7a75f8d54c3cd26e3792cdce0badcd68
TrangNgan248 <trangnganvirgo@gmail.com>
2023-01-03 13:54:52 +0700

    kadai1234

--- kadai1.rb
+++ kadai1.rb
@@ -2,12 +2,14 @@ class Menu
  attr_accessor :name
  attr_accessor :price
   # get_total_priceメソッドを定義してください
-
-
-
-  
-   return "#{self.name} #{total_price}vnd"
- end
+  def get_total_price(count)
+    if count >= 3
+      total_price = count*price - 10000
+    else
+      total_price = count*price
+    end
+    return "#{self.name} #{total_price}vnd"
+  end
 end
 
 menu1 = Menu.new
@@ -15,4 +17,6 @@ menu1.name = "Phở"
 menu1.price = 30000
 
 # menu1に対してget_total_priceメソッドを呼び出してください
-
+print ("Nhap count >")
+count = gets.to_i
+puts (menu1.get_total_price(count))
