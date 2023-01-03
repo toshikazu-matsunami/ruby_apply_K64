2e1745e4318097e8fc4e6f1217b568bd64225b50
giangleee <giangle602@gmail.com>
2023-01-03 12:59:54 +0700

    kaida1 2 3 4

--- kadai1.rb
+++ kadai1.rb
@@ -1,13 +1,14 @@
 class Menu
- attr_accessor :name
- attr_accessor :price
-  # get_total_priceメソッドを定義してください
-
-
-
-  
-   return "#{self.name} #{total_price}vnd"
- end
+  attr_accessor :name
+  attr_accessor :price
+   # get_total_priceメソッドを定義してください
+   def get_total_price(count)
+     total_price = count * self.price
+     if (count >= 3)
+       total_price -= 10000
+     end
+     return "#{self.name} #{total_price}vnd"
+   end
 end
 
 menu1 = Menu.new
@@ -15,4 +16,8 @@ menu1.name = "Phở"
 menu1.price = 30000
 
 # menu1に対してget_total_priceメソッドを呼び出してください
+puts "How many bolw of #{menu1.name} do you want to eat?"
+count = gets.chomp().to_i
+puts menu1.get_total_price(count)
 
+# End of file
