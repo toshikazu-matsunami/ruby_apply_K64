65b6d14e82f743cd7db88b555ead8151428d5148
vutuankiet4599 <vutuankiet4599@gmail.com>
2023-01-03 06:13:37 +0000

    fix duplicate in random

--- kadai1.rb
+++ kadai1.rb
@@ -23,4 +23,4 @@ end
 menu1 = Menu.new
 menu1.name = 'Phở'
 menu1.price = 30_000
-puts menu1.get_total_price('1.5')
+puts menu1.get_total_price('-12')
--- kadai4.rb
+++ kadai4.rb
@@ -1,30 +1,22 @@
 class Bingo
   attr_reader :column_b, :column_i, :column_n, :column_g, :column_o
   def initialize
-    @column_b = []
-    @column_i = []
-    @column_n = []
-    @column_g = []
-    @column_o = []
+    @column_b = (1..15).to_a.sample(5)
+    @column_i = (16..30).to_a.sample(5)
+    @column_n = (31..45).to_a.sample(5)
+    @column_g = (46..60).to_a.sample(5)
+    @column_o = (61..75).to_a.sample(5)
+    @column_n[2] = ''
+  end
 
-    5.times {
-      @column_b.push(rand(1..15))
-      @column_i.push(rand(16..30))
-      @column_n.push(rand(31..45))
-      @column_g.push(rand(46..60))
-      @column_o.push(rand(61..75))
-    }
+  def display
+    puts ' B| I| N| G| O'
+    (0..4).each do |i|
+      puts "#{@column_b[i].to_s.rjust(2)}|" + "#{@column_i[i].to_s.rjust(2)}|" + "#{@column_n[i].to_s.rjust(2)}|" + "#{@column_g[i].to_s.rjust(2)}|" + @column_o[i].to_s.rjust(2).to_s
+    end
   end
 end
 
 bingo = Bingo.new
 
-puts " B| I| N| G| O"
-
-for i in   0..4 do
-  if i != 2 
-    puts "#{bingo.column_b[i].to_s.rjust(2)}|"+"#{bingo.column_i[i].to_s.rjust(2)}|"+"#{bingo.column_n[i].to_s.rjust(2)}|"+"#{bingo.column_g[i].to_s.rjust(2)}|"+"#{bingo.column_o[i].to_s.rjust(2)}"
-  else
-     puts "#{bingo.column_b[i].to_s.rjust(2)}|"+"#{bingo.column_i[i].to_s.rjust(2)}|"+"  |"+"#{bingo.column_g[i].to_s.rjust(2)}|"+"#{bingo.column_o[i].to_s.rjust(2)}"
-  end
-end
+bingo.display
