b35abf9ca1171443048e0e59a179786f194284e6
Replit user <>
2023-01-03 05:50:34 +0000

    kadai3

--- kadai3.rb
+++ kadai3.rb
@@ -12,14 +12,32 @@ class Menu
   end
 end
 
+class Drink < Menu
+  attr_accessor :size
+
+  def initialize(name:,price:,size:)
+    super(name: name, price: price)
+    self.size = size
+  end
+
+  def showMenu
+    puts "#{self.name} #{self.price}vnd (#{self.size}サイズ)"
+  end
+end
+
 menu1 = Menu.new(name: "Phở", price: 30000)
 menu2 = Menu.new(name: "Bún chả", price: 40000)
 menu3 = Menu.new(name: "Bánh mì", price: 20000)
+
+drink1 = Drink.new(name: "Trà", price: 50000, size: "S")
+drink2 = Drink.new(name: "Trà", price: 100000, size: "M")
 
 menus = []
 menus.push(menu1)
 menus.push(menu2)
 menus.push(menu3)
+menus.push(drink1)
+menus.push(drink2)
 
 menus.each do |menu|
   menu.showMenu
