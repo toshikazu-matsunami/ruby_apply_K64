4508454d1710e5cb77e3bbd822e1da88361d8552
Rippo Kazami <90459878+Rippo12@users.noreply.github.com>
2023-01-05 08:52:31 +0000

    kadai2+3

--- kadai1.rb
+++ kadai1.rb
@@ -2,19 +2,15 @@ class Menu
   attr_accessor :name
   attr_accessor :price
   # get_total_priceメソッドを定義してください
-  def _get_total_price(count)
+  def get_total_price(count)
     total_price = price * count
-    if count > 3
-      total_price -= 10_000
-
-      "#{name} #{total_price}vnd"
-    end
+    total_price -= 10_000 if count > 3
+    "#{name} #{total_price}vnd"
   end
-
-  menu1 = Menu.new
-  menu1.name = 'Phở'
-  menu1.price = 30_000
-  count = 4
-  puts(menu1._get_total_price(count))
-  # menu1に対してget_total_priceメソッドを呼び出してください
 end
+menu1 = Menu.new
+menu1.name = 'Phở'
+menu1.price = 30_000
+count = 2
+puts(menu1._get_total_price(count))
+# menu1に対してget_total_priceメソッドを呼び出してください
