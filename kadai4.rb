104be86710aa036fae299d959dd91e3b5487830c
hiep464 <60540818+hiep464@users.noreply.github.com>
2023-01-07 17:07:38 +0700

    update kadai4

--- kadai4.rb
+++ kadai4.rb
@@ -1,14 +1,22 @@
 class Bingo
+    attr_accessor :b, :i, :n, :g, :o
     def initialize ()
-        @b = (1...15).to_a.sample(5)
-        @i = (16...30).to_a.sample(5)
-        @n = (31...45).to_a.sample(5)
-        @g = (46...60).to_a.sample(5)
-        @o = (61...75).to_a.sample(5)
+        self.b = (1...15).to_a.sample(5)
+        self.i = (16...30).to_a.sample(5)
+        self.n = (31...45).to_a.sample(5)
+        self.g = (46...60).to_a.sample(5)
+        self.o = (61...75).to_a.sample(5)
     end
 
     def generate_cart()
-        # できない
+        puts " B|  I|  N|  G|  O"
+        (0..4).each do |i|
+          if @b[i] < 10 then
+            puts " #{@b[i]}| #{@i[i]}| #{i == 2 ? '  ' : @n[i]}| #{@g[i]}| #{@o[i]}" # display number < 10
+          else
+            puts "#{@b[i]}| #{@i[i]}| #{i == 2 ? '  ' : @n[i]}| #{@g[i]}| #{@o[i]}"  #display number >= 10
+          end
+        end
     end
 end
 
