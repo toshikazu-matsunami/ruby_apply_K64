f636ba37c10d266502b65292ee12975735362553
dung <doantrungdung2001@gmail.com>
2023-01-08 23:38:42 +0700

    宿題

--- kadai1.rb
+++ kadai1.rb
@@ -1,18 +1,22 @@
 class Menu
- attr_accessor :name
- attr_accessor :price
-  # get_total_priceメソッドを定義してください
-
-
-
-  
-   return "#{self.name} #{total_price}vnd"
+  attr_accessor :name
+  attr_accessor :price
+   # get_total_priceメソッドを定義してください
+   def get_total_price(count)
+   total_price = self.price *count
+   if(count >= 3)
+     total_price-=10000
+   end
+     return "#{self.name} #{total_price}vnd"
+  end
  end
-end
-
-menu1 = Menu.new
-menu1.name = "Phở"
-menu1.price = 30000
-
-# menu1に対してget_total_priceメソッドを呼び出してください
-
+ 
+ menu1 = Menu.new
+ menu1.name = "Phở"
+ menu1.price = 30000
+ puts "Count:"
+ count = gets.to_i
+ puts menu1.get_total_price(count)
+ # menu1に対してget_total_priceメソッドを呼び出してください
+ 
+ 
