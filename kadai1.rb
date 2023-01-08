2ccc8bef5a480d76533302a635d3a5433c0fc217
kawasemi2k1 <bovainghiengnang2k1@gmail.com>
2023-01-08 20:48:00 +0700

    kadai 1 henshuu

--- kadai1.rb
+++ kadai1.rb
@@ -5,7 +5,7 @@ class Menu
   def get_total_price(count)
     total_price = self.price * count
     if count >= 3
-      total_price -= 10000
+      total_price -= (count)/3 * 10000
     end
     return "#{self.name} #{total_price}vnd"
   end
@@ -16,5 +16,5 @@ menu1.name = "Phở"
 menu1.price = 30000
 
 # menu1に対してget_total_priceメソッドを呼び出してください
-puts menu1.get_total_price(4)
+puts menu1.get_total_price(7)
 
