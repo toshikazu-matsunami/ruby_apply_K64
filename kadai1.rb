eab0db9d40a0ead38f80158b48deb8455e66d7fa
Replit user <>
2023-01-09 04:47:41 +0000

    kadai1234

--- kadai1.rb
+++ kadai1.rb
@@ -1,18 +1,21 @@
 class Menu
- attr_accessor :name
- attr_accessor :price
+  def initialize(name, price)
+    @name = name
+    @price = price
+  end
+
   # get_total_priceメソッドを定義してください
+  def get_total_price(count)
+    total_price = @price * count
+    total_price -= 10_000 if count >= 3
 
-
-
-  
-   return "#{self.name} #{total_price}vnd"
- end
+    puts "#{@name} #{total_price}vnd"
+  end
 end
 
-menu1 = Menu.new
-menu1.name = "Phở"
-menu1.price = 30000
+# Create a new Pho object with a price of 20000 vnd
+menu1 = Menu.new('Phở', 20_000)
 
-# menu1に対してget_total_priceメソッドを呼び出してください
-
+# Calculate the total price of 3 servings of Phở
+# Output the total price
+puts menu1.get_total_price(3)
