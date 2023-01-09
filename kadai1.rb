2f0f424c08a3344fa42f9f3f838f06f5271241e0
letuanhung10022001 <103104548+letuanhung10022001@users.noreply.github.com>
2023-01-09 11:29:06 +0700

    Update kadai1.rb

--- kadai1.rb
+++ kadai1.rb
@@ -1,18 +1,21 @@
 class Menu
- attr_accessor :name
- attr_accessor :price
+  attr_accessor :name
+  attr_accessor :price
   # get_total_priceメソッドを定義してください
-
-
-
-  
-   return "#{self.name} #{total_price}vnd"
- end
+  def get_total_price(num)
+    total_price = if num >= 3
+                    num * price - 10_000
+                  else
+                    num * price
+                  end
+    "#{name} #{total_price}vnd"
+  end
 end
 
 menu1 = Menu.new
-menu1.name = "Phở"
-menu1.price = 30000
+menu1.name = 'Phở'
+menu1.price = 30_000
 
 # menu1に対してget_total_priceメソッドを呼び出してください
+puts menu1.get_total_price(1)
 
