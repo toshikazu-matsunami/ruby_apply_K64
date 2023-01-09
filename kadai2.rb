2107e25d9012f850b4b438b2757864f9fbf7e079
Replit user <>
2023-01-09 05:28:52 +0000

    kadai 1,2,3,4

---
+++ kadai2.rb
@@ -0,0 +1,20 @@
+class Menu
+  attr_accessor :name
+  attr_accessor :price
+  # get_total_priceメソッドを定義してください
+  def initialize(name, price)
+    @name = name
+    @price = price
+    puts "#{self.name} #{self.price} vnd"
+  end
+end
+
+menu1 = ['Phở', 30_000]
+menu2 = ['Bún chả', 40_000]
+menu3 = ['Bánh mì', 20_000]
+
+menus = [menu1, menu2, menu3]
+
+menus.each do |name, price|
+  Menu.new name, price
+end
---
+++ kadai3.rb
@@ -0,0 +1,36 @@
+class Menu
+  def initialize(name, price)
+    @name = name
+    @price = price
+  end
+
+  def display
+    puts "#{@name} #{@price}vnd"
+  end
+end
+
+class Drink < Menu
+  attr_accessor :size
+
+  def initialize(name, price, size)
+    super(name, price)
+    @size = size
+  end
+
+  def display
+    puts "#{@name} #{@price}vnd (#{@size})"
+  end
+end
+
+# Define the menu items
+menu1 = Menu.new('Phở', 30_000)
+menu2 = Menu.new('Bún chả', 40_000)
+menu3 = Menu.new('Bánh mì', 20_000)
+drink1 = Drink.new('Trà', 5000, 'Sサイズ')
+drink2 = Drink.new('Trà', 10_000, 'Mサイズ')
+
+menus = [menu1, menu2, menu3, drink1, drink2]
+
+menus.each do |menu|
+  menu.display
+end
